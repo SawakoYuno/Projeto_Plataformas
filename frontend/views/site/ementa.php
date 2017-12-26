4d99ec5481f481dab26bf5d1a4ea4e4be1e207e5
Joaquim <2160442@my.ipleiria.pt>
2017-12-26 16:06:46 +0000

    Update 2

---
+++ frontend/views/site/ementa.php
@@ -0,0 +1,77 @@
+<?php
+
+/* @var $this yii\web\View */
+
+//$this->title = 'My Yii Application';
+?>
+<div class="jumbotron">
+
+    <div class="col-lg-2">
+        <a href="http://localhost/Projeto_Plataformas/frontend/web"><img src="logo.png" style="width:42px;height:42px;border:0;" alt="Responsive Image" ></a> <!--Por acabar -->
+    </div>
+
+    <div class="col-lg-2">
+        <p><a class="btn btn-lg" href="site\ementa">Ementa</a></p>
+    </div>
+
+    <div class="col-lg-2">
+        <p><a class="btn btn-lg" href="site\reservas">Reservas</a></p>
+    </div>
+
+    <div class="col-lg-2">
+        <p><a class="btn btn-lg" href="site\sobre">Sobre</a></p>
+    </div>
+
+    <div class="col-lg-2">
+        <p><a class="btn btn-lg" href="site\contactos">Contactos</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Entradas</h2>
+
+                <p>(Adicionar Imagens).</p> <!-- COLOCAR IMAGEM OU ANIMAÇÃO COM O PRATO DO DIA -->
+
+
+            </div>
+            <div class="col-lg-4">
+                <h2>Prato do dia</h2>
+
+                <p>(Adicionar Imagens)</p>
+
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Carnes</h2>
+
+                <p>(Adicionar Imagens)</p>
+
+            </div>
+
+            <div class="col-lg-4">
+                    <h2>Peixes</h2>
+
+                <p>(Adicionar Imagens)</p>
+
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Sobremesas</h2>
+
+                <p>(Adicionar Imagens)</p>
+
+            </div>
+
+            <div class="col-lg-4">
+                <h2>Sopas</h2>
+
+                <p>(Adicionar Imagens)</p>
+
+            </div>
+
+        </div>
+
+    </div>
+</div>
