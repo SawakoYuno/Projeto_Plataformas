925c4dc6398695c4e7b928912fe38012ed02a644
Joaquim <2160442@my.ipleiria.pt>
2017-12-29 15:59:31 +0000

    Carousel não completo e falta a ementa.

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,4 +1,6 @@
 <?php
+
+use yii\bootstrap\Carousel;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\Url;
@@ -17,6 +19,21 @@ $this->title = 'Vanilla';
 
         <!-- <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p> -->
     </div>
+
+     <?php
+    echo Carousel::widget([
+        'items' => [
+
+            ['content'=>  Html::img('@web/img/bife_a_portuguesa.jpg', ['id'=>'imagem-slide'])],
+            ['content'=>  Html::img('@web/img/salmao_grelhado_com_legumes.jpg', ['id' => 'imagem-slide'])],
+            ['content'=>  Html::img('@web/img/lombinhos_de_porco_preto.jpg', ['id' => 'imagem-slide'])],
+            ],
+    ]);
+    ?>
+
+
+
+
 
     <div class="site-index">
 
