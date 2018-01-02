03bca54e6f76cfe97392d7a215c43dd182cd9d25
Joaquim <2160442@my.ipleiria.pt>
2018-01-02 14:15:33 +0000

    Index coisado

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -20,16 +20,7 @@ $this->title = 'Vanilla';
         <!-- <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p> -->
     </div>
 
-     <?php
-    echo Carousel::widget([
-        'items' => [
 
-            ['content'=>  Html::img('@web/img/bife_a_portuguesa.jpg', ['id'=>'imagem-slide'])],
-            ['content'=>  Html::img('@web/img/salmao_grelhado_com_legumes.jpg', ['id' => 'imagem-slide'])],
-            ['content'=>  Html::img('@web/img/lombinhos_de_porco_preto.jpg', ['id' => 'imagem-slide'])],
-            ],
-    ]);
-    ?>
 
 
 
@@ -40,28 +31,19 @@ $this->title = 'Vanilla';
 
             <div style="text-align: center">
                 <h2><strong>Especialidades</strong></h2>
+                <?php
+                echo Carousel::widget([
+                    'items' => [
+
+                        ['content'=>  Html::img('@web/img/bife_a_portuguesa.jpg', ['id'=>'imagem-slide'])],
+                        ['content'=>  Html::img('@web/img/salmao_grelhado_com_legumes.jpg', ['id' => 'imagem-slide'])],
+                        ['content'=>  Html::img('@web/img/lombinhos_de_porco_preto.jpg', ['id' => 'imagem-slide'])],
+                    ],
+                ]);
+                ?>
                     <!-- <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p> -->
             </div>
-        <table style="width:80%">
 
-            <tr>
-                <td>
-                    <?=Html::img('@web/img/bife_a_portuguesa.jpg', ['id' => 'imagem-teste']);?>
-                    <p id="texto">Bife à Portuguesa</p>
-                </td>
-                <td>
-                    <?=Html::img('@web/img/salmao_grelhado_com_legumes.jpg', ['id' => 'imagem-teste']);?>
-                    <p id="texto">Salmão Grelhado com Legumes</p>
-                </td>
-                <td>
-                    <?=Html::img('@web/img/lombinhos_de_porco_preto.jpg', ['id' => 'imagem-teste']);?>
-                    <p id="texto">Lombinhos de Porco Preto</p>
-                </td>
-            </tr>
-
-
-            </tr>
-        </table>
 
     </div>
 
