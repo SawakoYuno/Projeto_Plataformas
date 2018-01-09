9ca19c1abb108fb6b23443c2724b9279c9322bc5
Joaquim <2160442@my.ipleiria.pt>
2018-01-09 10:53:21 +0000

    Ementa detalhes a fazer

--- frontend/views/artigo/detalhes.php
+++ frontend/views/artigo/detalhes.php
@@ -22,35 +22,38 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="artigo-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
+<?php
+$artigos = Artigo::find()
+    ->join('JOIN', 'tipo_artigo', 'tipo_artigo.id = artigo.id_tipo_artigo')
+    ->where('artigo.id_tipo_artigo =1')
+    ->all();
 
 
+
+?>
     <div class="row">
-        <div class="col-6 col-md-5">.col-6 .col-md-4</div>
-        <div class="col-6 col-md-5">.col-6 .col-md-4</div>
+<?php
+foreach ($artigos as $artigo) {
+    $tipoartigo = TipoArtigo::find()
+        ->where(['id' => $artigo->id_tipo_artigo])
+        ->one();
+    ?>
+        <div class="col-6 col-md-3">
+            <?= Html::img('/Projeto_Plataformas/images/artigos/'. $artigo->imagem_artigo, ['id' => 'imagem-ementa'])?>
+
+
+
+        </div>
+<?php }?>
+        <div class="col-6 col-md-5">
+              <?= $artigo->nome;?>
+              <?= $artigo->detalhes;?>
+              <?= $artigo->preco;?>
+        </div>
 
 
     </div>
 </div>
 
 
-<?php
-$artigos = Artigo::find()->all();
 
-foreach ($artigos as $artigo) {
-    $tipoartigo = TipoArtigo::find()
-        ->where(['id' => $artigo->id_tipo_artigo])
-        ->one();
-    ?>
-    <tr>
-        <td>  <?= $artigo->id;?></td>
-        <td>  <?= $tipoartigo->nome;?></td>
-        <td>  <?= $artigo->nome;?></td>
-        <td>  <?= $artigo->detalhes;?></td>
-        <td>  <?= $artigo->preco;?></td>
-        <td>  <?= $artigo->quantidade;?></td>
-        <td> <?= Html::img('/Projeto_Plataformas/images/artigos/'. $artigo->imagem_artigo, ['id' => 'imagem-teste']);?></td>
-
-
-
-    </tr>
-<?php }?>
